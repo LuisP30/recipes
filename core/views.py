342204d28453b143c9aad270efcbe176137d1663
LuisP30 <pimenta.lh2@gmail.com>
2023-05-24 09:59:19 -0300

    nesting urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+    return render(request, 'global/index.html')
+
+def cadastro(request):
+    return HttpResponse('Cadastro')
+
+def contato(request):
+    return HttpResponse('Contato')
 # Create your views here.

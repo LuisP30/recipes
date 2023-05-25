34b2a4f46463377f37945843f8ce5c64834f4f2d
LuisP30 <pimenta.lh2@gmail.com>
2023-05-25 14:22:56 -0300

    Updated views.py

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luis Henrique'
+    })
 # Create your views here.

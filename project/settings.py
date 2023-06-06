2e59354b64a6b19b5946782dca7c335c11b427fa
LuisP30 <pimenta.lh2@gmail.com>
2023-06-06 10:49:12 -0300

    command collectstatic

--- project/settings.py
+++ project/settings.py
@@ -124,7 +124,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
- 
+
+# python manage.py collectstatic
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

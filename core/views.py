a511d7f2592dd0e05833ae55c645249582cf8f47
LuisP30 <pimenta.lh2@gmail.com>
2023-06-19 08:32:20 -0300

    commit

--- core/views.py
+++ core/views.py
@@ -4,4 +4,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luis Henrique'
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Luis Henrique'
+    })
 # Create your views here.
